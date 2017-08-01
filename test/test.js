178ddd34fb743df5a339291c92eb1682f090f0bb
Macrichie <makanjuolakunle@gmail.com>
2017-08-01 06:35:05 +0100

    Added if array element is not integer test case

--- test/test.js
+++ test/test.js
@@ -6,4 +6,8 @@ describe("To determine the sequence of an empty array: ", function() {
     it("should return 0 for an empty array", function() { 
         assert.equal(aritGeo(["This is Andela"]), 0);
     });
+
+    it("should return 0 if array element is not integer", function() { 
+        assert.equal(aritGeo(["This is Andela"]), 0);
+    });
 });
