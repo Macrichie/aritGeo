1287d41f6cbe67d6301b3332e4aab0b4c6e7e199
Macrichie <makanjuolakunle@gmail.com>
2017-08-01 02:21:42 +0100

    Created aritGeo.js and test.js files

---
+++ aritGeo.js
@@ -0,0 +1,52 @@
+'use strict'
+function aritGeo(arr) { 
+    let diff = arr[1] - arr[0],
+        ratio = arr[1] / arr[0],
+
+        arith = true,
+        geo = true,
+        i;
+
+    if(arr == null || !Array.isArray(arr)) {
+        return 0;
+    }
+
+    let length = arr.length;
+    if(length <= 1){
+        return 0;
+    }
+
+    for(i = 0; i < arr.length - 1; i++) {
+    
+        if( arr[i + 1] - arr[i] != diff ) {
+            arith = false;
+        }
+
+        if(arr[i + 1] / ratio != arr[i]) {
+            geo = false;
+        }      
+    }
+// check if array elements are integers
+    if (isNaN(arr[i])) {
+        return 0;
+    }
+
+    if(arith === true) {
+        return "arithmetic";
+    } else if(geo === true) {
+        return" geometric";
+    } else {
+        return -1;
+    }
+}
+
+console.log(aritGeo([10, 30, 90, 270, 810, 2430]));
+console.log(aritGeo([3,9,15,21,27,28]));
+console.log(aritGeo([3,9,15,21,27]));
+console.log(aritGeo([4,2,1,0.5]));
+console.log(aritGeo([3]));
+console.log(aritGeo(2));
+console.log(aritGeo([3, -2, 1]));
+
+
+module.exports = aritGeo;
